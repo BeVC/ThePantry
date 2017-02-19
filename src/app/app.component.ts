2bb794a42460913c9397ad478e3cf658136ce6bf
BeVC <Bernard_Van_Couwenberghe@hotmail.com>
2017-02-19 21:37:59 +0100

    navbar displays user name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,18 @@
+//#region import
+// ANGULAR
 import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+
+// RXJS
+import { Subscription } from "rxjs"
+
+// BROKER
+import { UserBroker } from "./broker/user.service";
+
+// MODELS
+import { User } from "./models/user"
+
+//#endregion
 
 @Component({
   selector: 'app-root',
@@ -10,18 +23,35 @@ import { Router } from "@angular/router";
 export class AppComponent {
   title = 'app works!';
   loggedIn: boolean = false;
+  user: User;
+
+
+  // BROKER SUBSCRIPTIONS
+  private _userLoggedSubscription: Subscription
 
   constructor(
-    private router: Router
-  ){}
+    private router: Router,
+    private userBroker: UserBroker
+  ) { }
 
-  ngOnInit(){
-    if(!this.loggedIn){
-    let path = "login";
-    this.router.navigate([path]);
-    } else{
+  //#region Angular Events
+  ngOnInit() {
+    this._userLoggedSubscription = this.userBroker.onUserLoggedAsync()
+      .subscribe(user => {
+        this.user = user;
+      });
+
+    if (!this.loggedIn) {
+      let path = "login";
+      this.router.navigate([path]);
+    } else {
       let path = "hub";
-    this.router.navigate([path]);
+      this.router.navigate([path]);
     }
   }
+
+  ngOnDestroy() {
+    this._userLoggedSubscription.unsubscribe();// -> probably useless
+  }
+  //#endregion
 }
--- src/app/shared/ui/navbar/navbar.component.ts
+++ src/app/shared/ui/navbar/navbar.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+//#region import
+// ANGULAR
+import { Component, OnInit, OnDestroy, ViewEncapsulation, Input, OnChanges } from '@angular/core';
+import { Router } from "@angular/router";
+
+// RXJS
+import { Subscription } from "rxjs";
+
+// MODEL
+import { User } from "../../../models/user";
+//#endregion
 
 @Component({
   selector: 'app-navbar',
@@ -6,11 +16,36 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./navbar.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnChanges {
+  // UI PROPERTIES
+  name: string = "";
 
-  constructor() { }
+  @Input("user") user: User
 
+  constructor(
+    private router: Router,
+  ) { }
+
+  //#region Angular Events
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+  }
+
+  ngOnChanges(){
+    if(this.user){
+      this.name = this.user.firstName + " " + this.user.lastName;
+    }else{
+      this.name = "";
+    }
+  }
+  //#endregion
+
+  //#region UI Events
+  uiOnReturnToHubClicked() {
+    let path: "/hub";
+    this.router.navigate([path]);
+  }
+  //#endregion
 }
