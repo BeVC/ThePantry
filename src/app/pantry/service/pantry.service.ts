c388d49c0112d48cd09ce2c1b68550044811d85e
BeVC <Bernard_Van_Couwenberghe@hotmail.com>
2017-03-07 21:20:40 +0100

    pantry service get all items

--- src/app/pantry/service/pantry.service.ts
+++ src/app/pantry/service/pantry.service.ts
@@ -1,11 +1,65 @@
 //#region import
 // ANGULAR
 import { Injectable } from '@angular/core';
+import { Headers, Http, Response } from "@angular/http";
+
+// RXJS
+import { Subject } from "rxjs/Subject";
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/toPromise';
+import "rxjs/Rx";
+
+// MODELS
+import { Apiresponse } from "../../models/apiresponse"
 //#endregion
 
 @Injectable()
 export class PantryService {
 
-  constructor() { }
+  private pantryItemsUrl = "app/pantryItems";
+
+  constructor(
+    private http: Http
+  ) { }
+
+  getPantryItemsAsync(pantryId: string): Observable<Apiresponse<any[]>> {
+    return this.http.get(this.pantryItemsUrl)
+      .map(response => {
+        return this.mapToApiResponse(response);
+      })
+      .do(response => {
+        response;
+      })
+      .catch(this.handleError)
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.data || {};
+  }
+
+  private handleError(error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || "";
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ""} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+
+  private mapToApiResponse<T>(response: Response): Apiresponse<T> {
+    let retour = new Apiresponse<T>();
+
+    retour.ok = response.ok;
+    retour.status = response.status;
+    retour.statusText = response.statusText;
+    retour.data = response["_body"].data;
+
+    return retour;
+  }
 
 }
--- src/app/pantry/ui/pantry-list/pantry-list.component.ts
+++ src/app/pantry/ui/pantry-list/pantry-list.component.ts
@@ -1,4 +1,16 @@
+//#region import
+// ANGULAR
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+// SERVICE
+import { PantryService } from "../../service/pantry.service";
+
+// BROKER
+import { UserBroker } from "../../../broker/user.service";
+
+// MODELS
+import { User } from "../../../models/user";
+//#endregion
 
 @Component({
   selector: 'app-pantry-list',
@@ -7,10 +19,42 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class PantryListComponent implements OnInit {
+  // UI PROPERTIES
+  pantryListCol: any[] = [];
+  user: User;
 
-  constructor() { }
+  // CLASS PROPERTIES
 
+  constructor(
+    private pantryService: PantryService,
+    private userBroker: UserBroker
+  ) { }
+
+  //#region Angular Events
   ngOnInit() {
+    this.user = this.userBroker.getUser();
   }
+  //#endregion
 
+  //#region UI Events
+  test() {
+    this.getAllPantryItems();
+  }
+  //#endregion
+
+  //#region Public Class Methods
+  //#endregion
+
+  //#region Private Class Methods
+  private getAllPantryItems() {
+    let pantryId = this.user.pantryId;
+    this.pantryService.getPantryItemsAsync(pantryId)
+      .subscribe(response => {
+        if (response.ok) {
+          let col = response.data;
+          this.pantryListCol = col.filter(item => item.pantryId == pantryId);
+        }
+      });
+  }
+  //#endregion
 }
